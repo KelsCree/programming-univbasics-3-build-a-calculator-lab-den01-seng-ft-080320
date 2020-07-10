de8e6d5fb2b99062b766a9c4e54245171ec3c3e8
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-10 21:52:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,17 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-div = num1 / num2
-div
+  div = num1 / num2
+  div
 end
 
 def multiplication(num1, num2)
   times = num1 * num2
-times
+  times
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
